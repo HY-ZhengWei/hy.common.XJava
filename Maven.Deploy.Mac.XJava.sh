c7d802475754c20cad152edc6dbe4ea78374f717
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-30 10:27:03 +0800

    3.9.4
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

---
+++ Maven.Deploy.Mac.XJava.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn deploy:deploy-file -Dfile=xjava.jar -DpomFile=./src/META-INF/maven/org/hy/common/xjava/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
