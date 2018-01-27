8ebc9a6d1d460844a3e769281b70791aa38af4f7
HY-ZhengWei <hy.zhengwei@qq.com>
2018-01-27 11:25:51 +0800

    3.9.8
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- src/org/hy/common/xml/XSQLProxy.java
+++ src/org/hy/common/xml/XSQLProxy.java
@@ -37,6 +37,7 @@ import org.hy.common.xml.plugins.XSQLGroupResult;
  * @version     v1.0
  *              v1.1  2018-01-20  修复：@Xsql.names() 与 @Xparam.name() 在判定合计数量上的问题。
  *              v1.2  2018-01-25  添加：对查询SQL的记录行数功能的支持。
+ *              v1.3  2018-01-27  添加：批量数据的更新功能。
  */
 public class XSQLProxy implements InvocationHandler ,Serializable
 {
@@ -813,7 +814,15 @@ public class XSQLProxy implements InvocationHandler ,Serializable
         }
         else
         {
-            v_Ret = i_XSQL.executeUpdate(v_Params);
+            if ( v_Params instanceof List )
+            {
+                // 批量数据的更新功能  2018-01-27
+                v_Ret = i_XSQL.executeUpdates((List<?>)v_Params);
+            }
+            else
+            {
+                v_Ret = i_XSQL.executeUpdate(v_Params);
+            }
         }
         
         succeedLog(i_Anno ,v_Ret);
